0a8130b3baf05d7605f38c87f3c151fc0e113774
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-04-01 18:36:37 -0500

    add/feature/firstSection in process...

--- src/components/FirstSection/index.js
+++ src/components/FirstSection/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./FirstSection.modules.css";
+import styles from "./FirstSection.module.css";
 
 const FirstSection = () => {
 	return (
@@ -13,7 +13,36 @@ const FirstSection = () => {
 				</p>
 			</div>
 			<section className={styles.tableContainer}>
-				<div> tablas</div>
+				<div className={styles.currencyContainer}>
+					<h3 className={styles.h3}>Monedas</h3>
+					<div className={styles.table}>
+						<table>
+							<tr className={styles.tableRow}>
+								<td className={styles.tableDataLeftTop}>Bitcoin</td>
+								<td className={styles.tableDataRightTop}>
+									$1.96 <span className={styles.imgTrending}></span>
+								</td>
+							</tr>
+							<tr className={styles.tableRow}>
+								<td className={styles.tableDataLeft}>Etherium</td>
+								<td className={styles.tableDataRight}>$0.07</td>
+							</tr>
+							<tr className={styles.tableRow}>
+								<td className={styles.tableDataLeft}>Ripple</td>
+								<td className={styles.tableDataRight}>$2.15</td>
+							</tr>
+							<tr className={styles.tableRow}>
+								<td className={styles.tableDataLeftBottom}>Stellar</td>
+								<td className={styles.tableDataRightBottom}>$4.96</td>
+							</tr>
+						</table>
+					</div>
+					<div className={styles.currencyDate}>
+						<p>
+							<b> Actulizado:</b> 19 Julio 23:45{" "}
+						</p>
+					</div>
+				</div>
 			</section>
 		</section>
 	);
