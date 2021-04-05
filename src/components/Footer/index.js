a85053133b3d2018bf07e42943baa32e039d0545
jorge arguelles <jorgeariasarguelles@gmail.com>
2021-04-05 14:07:27 -0500

    add/feature/footer finish

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,28 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+import logoFooter from "../../icons/logoFooter.svg";
+
 const Footer = () => {
 	return (
-		<>
-			<p>Footer</p>
-		</>
+		<footer className={styles.footer}>
+			<section className={styles.leftSection}>
+				<ul>
+					<li>
+						<a href="#">LinkedIn</a>
+					</li>
+					<li>
+						<a href="#">Crounchbase</a>
+					</li>
+					<li>
+						<a href="#">Hackernew</a>
+					</li>
+				</ul>
+			</section>
+			<section className={styles.rightSection}>
+				<img src={logoFooter} alt="Logo de batata" />
+			</section>
+		</footer>
 	);
 };
 
